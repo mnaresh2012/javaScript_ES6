aaf9511a2412851ca9e2fc82713116cc65954c71
Naresh_Mahanta <naresh.mahanta@valtech.com>
2022-10-27 08:08:42 +0530

    updated the array

--- es6.js
+++ es6.js
@@ -1,1 +1,89 @@
-const testData = ['Naresh', 'Suresh', 'Ajay', 'Debasis', 'Sanjay', 'Ranjan'];
+const companies= [
+  {name: "Company One", category: "Finance", start: 1981, end: 2004},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+//reference - https://jsfiddle.net/bradtraversy/e5rb0x0a/
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+/*let - const - var
+var: 
+	- hoisted (always declared at top of scope, global if none)
+    - function scope
+
+    var greeter = "hey hi";
+    greeter = "say Hello instead";
+    
+
+	** Problem with var
+    var greeter = "hey hi";
+    var times = 4;
+
+    if (times > 3) {
+        var greeter = "say Hello instead"; 
+    }
+    
+    console.log(greeter) // "say Hello instead"
+
+
+let:
+    - block scope
+    - can be updated with new value
+    - not redeclarable
+
+    let greeting = "say Hi";
+    greeting = "say Hello instead";
+
+const: 
+    - block scope
+    - not reassignable
+    - not redeclarable
+
+
+
+const greeting = "say Hi";
+    greeting = "say Hello instead";// error: Assignment to constant variable.
+
+var userAge = 31;
+*/
+
+//Default Parameter
+
+//Rest Parameter
+
+//spread Operator
+
+//Object Literal
+
+//Template Literal
+
+//Destructuring
+
+//ES6 Module
+
+//ES6 Classes
+
+//Arrow Functions
+
+//Symbol
+
+//Iterators & Generators
+
+//Promises
+
+//ES6 Collection ( Map, Set )
+
+
+//Array 
+
+//String
+
+//Object
