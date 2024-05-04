96352647bb53522778de79c6d396f0b7f5f7268a
Naresh_Mahanta <naresh.mahanta@valtech.com>
2024-05-04 17:00:37 +0530

    javascript import and export syntax

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log('foo');
+// import Multiplication, {PI, Substraction, Addition} from "./module";
+
+// console.log(Addition(5, 5));
+// console.log(Substraction(10, 6));
+// console.log(PI);
+// console.log(Multiplication(5, 8));
+
+
+
+import * as MathModule from './module';
+
+console.log(MathModule.Addition(5, 5));
+console.log(MathModule.Substraction(10, 6));
+console.log(MathModule.PI);
+
+console.log(MathModule.default(5, 8));
---
+++ js/module.js
@@ -0,0 +1,12 @@
+export const PI = 3.14;
+export function Addition(x, y) {
+    return x + y;
+}
+
+export function Substraction (x, y) {
+    return x - y;
+}
+
+export default function Multiplication(x, y) {
+    return x * y;
+}
