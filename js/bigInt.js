565d65f90c3f3c7f12631ade403424d2d7e5d2d1
Naresh_Mahanta <naresh.mahanta@valtech.com>
2024-05-05 11:54:59 +0530

    javascript bigint

---
+++ js/bigInt.js
@@ -0,0 +1,10 @@
+let x = BigInt(45); // one way to declare bigInt
+let y = 50n; // second way of declaring bigInt
+
+// we can'nt do arithmetic operation between number and bigInt
+// bigInt can not store floating value
+// it is used to store bigger integer value
+
+// document.write(x * y);
+
+typeof x // bigint
