fb40d9611d65515d8b89cc03d1947b8660ac3b01
Naresh_Mahanta <naresh.mahanta@valtech.com>
2024-09-01 17:34:12 +0530

    ES6 string literal

--- js/template_literal.js
+++ js/template_literal.js
@@ -1,3 +1,12 @@
-let userName = 'John';
+// In ES6 two way we can define a string
 
-console.log(`Hello! ${userName}.`);
+let userName = 'Naresh'; // old way
+
+let newUserName = `Naresh Chandra Mahanta`;
+console.log(newUserName);
+
+const greet = (msg) => {
+    console.log(`Hi! ${msg} `);
+}
+
+greet('Naresh'); // output: Hi! Naresh
